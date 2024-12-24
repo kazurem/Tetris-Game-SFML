b66f68782a67814246cc1ec27352addee00ac06b
AuroraChasma <astraignis1@gmail.com>
2024-12-24 09:37:04 +0500

    adding another font

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Tetris-Game-SFML
+
+I will be making a tetris game using the graphics library SFML (Simple and Fast Media Layer).
+
--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 
 !bin/bash
 
-g++ -std=c++17 Main.cpp -o tetris -lsfml-graphics -lsfml-window -lsfml-system -lsfml-audio  && ./tetris
+g++ -std=c++17 main.cpp -o tetris -lsfml-graphics -lsfml-window -lsfml-system -lsfml-audio  && ./tetris
